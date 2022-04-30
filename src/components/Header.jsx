5ccd84d5b71bf503a0a9f4a59813b4dbc70361eb
sava642 <101993338+sava642@users.noreply.github.com>
2022-04-30 16:54:30 +0200

    header commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
 	return <nav className="deep-purple darken-2">
 		<div className="nav-wrapper">
-			<a href="https://www.google.com/webhp?hl=ru&sa=X&ved=0ahUKEwiAjqLUmKr3AhXolosKHQZIC70QPAgI" className="brand-logo">React Shop</a>
+			<a href="#!" className="brand-logo">React Shop</a>
 			<ul id="nav-mobile" className="right hide-on-med-and-down">
 				<li><a href="sass.html">Top</a></li>
 				<li><a href="badges.html">Noveltis</a></li>
