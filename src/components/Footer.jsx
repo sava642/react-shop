1766f87ad2758f97618459303b6c1eb4f3fbc4f7
sava642 <101993338+sava642@users.noreply.github.com>
2022-05-03 18:52:32 +0200

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
 		<div className="footer-copyright deep-purple accent-1">
 			<div className="container">
 				© {new Date().getFullYear()} Copyright Text
-				<a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+				<a className="grey-text text-lighten-4 right" href="https://sava642.github.io/react-shop/" target="_blank" rel="noreferrer">Repo</a>
 			</div>
 		</div>
 	</footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
 			<ul id="nav-mobile" className="right hide-on-med-and-down">
 				<li><a href="sass.html">Top</a></li>
 				<li><a href="badges.html">Noveltis</a></li>
-				<li><a href="!#">Contacts</a></li>
+				<li><a href="https://sava642.github.io/react-shop/" target="_blank" rel="noreferrer">Repo</a></li>
 			</ul>
 		</div>
 	</nav>
